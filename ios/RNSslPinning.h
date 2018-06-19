0920e722ea87fe30493c491851d1bc2dc0f0c1d1
Maxim Toyberman <maxtoyberman@gmail.com>
2018-06-19 17:55:08 +0300

    Update RNSslPinning.h

--- ios/RNSslPinning.h
+++ ios/RNSslPinning.h
@@ -1,9 +1,10 @@
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
+#import <Foundation/Foundation.h>
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
 #endif
+#import "RCTLog.h"
 
 @interface RNSslPinning : NSObject <RCTBridgeModule>
 
