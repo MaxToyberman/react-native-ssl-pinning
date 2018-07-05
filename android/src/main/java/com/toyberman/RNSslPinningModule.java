90b22bdda01c91f558c0e8bbe6bfa2e90bd9bb23
Maxim Toyberman <>
2018-07-05 11:40:38 +0300

    Fix android cookies handling

--- android/src/main/java/com/toyberman/RNSslPinningModule.java
+++ android/src/main/java/com/toyberman/RNSslPinningModule.java
@@ -10,18 +10,23 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
-import com.franmontiel.persistentcookiejar.PersistentCookieJar;
-import com.franmontiel.persistentcookiejar.cache.SetCookieCache;
-import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor;
+import com.facebook.react.bridge.WritableNativeMap;
 import com.toyberman.Utils.OkHttpUtils;
 
 import org.json.JSONException;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 
+import okhttp3.Cookie;
 import okhttp3.CookieJar;
 import okhttp3.Headers;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -32,14 +37,89 @@ public class RNSslPinningModule extends ReactContextBaseJavaModule {
     private static final String OPT_SSL_PINNING_KEY = "sslPinning";
 
     private final ReactApplicationContext reactContext;
+    private final HashMap<String, List<Cookie>> cookieStore;
     private CookieJar cookieJar = null;
     private OkHttpClient client;
 
     public RNSslPinningModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+        cookieStore = new HashMap<>();
+        cookieJar = new CookieJar() {
 
-        cookieJar = new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(this.reactContext));
+
+            @Override
+            public void saveFromResponse(HttpUrl url, List<Cookie> unmodifiableCookieList) {
+
+                for (Cookie cookie : unmodifiableCookieList) {
+                    setCookie(url, cookie);
+                }
+
+            }
+
+            @Override
+            public List<Cookie> loadForRequest(HttpUrl url) {
+                List<Cookie> cookies = cookieStore.get(url.host());
+                return cookies != null ? cookies : new ArrayList<Cookie>();
+            }
+
+
+            public void setCookie(HttpUrl url, Cookie cookie) {
+
+                final String host = url.host();
+
+                List<Cookie> cookieListForUrl = cookieStore.get(host);
+                if (cookieListForUrl == null) {
+                    cookieListForUrl = new ArrayList<Cookie>();
+                    cookieStore.put(host, cookieListForUrl);
+                }
+                putCookie(cookieListForUrl, cookie);
+
+            }
+
+            private void putCookie(List<Cookie> storedCookieList, Cookie newCookie) {
+
+                Cookie oldCookie = null;
+                for (Cookie storedCookie : storedCookieList) {
+
+                    // create key for comparison
+                    final String oldCookieKey = storedCookie.name() + storedCookie.path();
+                    final String newCookieKey = newCookie.name() + newCookie.path();
+
+                    if (oldCookieKey.equals(newCookieKey)) {
+                        oldCookie = storedCookie;
+                        break;
+                    }
+                }
+                if (oldCookie != null) {
+                    storedCookieList.remove(oldCookie);
+                }
+                storedCookieList.add(newCookie);
+            }
+        };
+
+    }
+
+    public static String getDomainName(String url) throws URISyntaxException {
+        URI uri = new URI(url);
+        String domain = uri.getHost();
+        return domain.startsWith("www.") ? domain.substring(4) : domain;
+    }
+
+    @ReactMethod
+    public void getCookies(String domain, final Promise promise) {
+        try {
+            WritableMap map = new WritableNativeMap();
+
+            List<Cookie> cookies = cookieStore.get(getDomainName(domain));
+            for (Cookie cookie : cookies) {
+                map.putString(cookie.name(), cookie.value());
+            }
+
+            promise.resolve(map);
+        } catch (Exception e) {
+            promise.reject(e);
+        }
     }
 
     @ReactMethod
