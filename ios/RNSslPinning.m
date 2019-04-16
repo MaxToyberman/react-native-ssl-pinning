98bcb0fcc303204539a1c9579f9c8fa5d5c3d69d
Maxim Toyberman <maxtoyberman@gmail.com>
2019-04-16 15:26:48 +0300

    Fix GET request bug when no body exists

--- ios/RNSslPinning.m
+++ ios/RNSslPinning.m
@@ -231,12 +231,11 @@ RCT_EXPORT_METHOD(fetch:(NSString *)url obj:(NSDictionary *)obj callback:(RCTRes
                 NSData *data = [obj[@"body"] dataUsingEncoding:NSUTF8StringEncoding];
                 [request setHTTPBody:data];
                 
-                [self performRequest:manager request:request callback:callback ];
                 //TODO: if no body
             }
             
         }
-
+        [self performRequest:manager request:request callback:callback ];
     }
     else {
         
