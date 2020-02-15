2ad8ed2cbbe4afa7951a8f795170734ac52bcfbe
Maxim Toyberman <maxtoyberman@gmail.com>
2020-02-15 17:19:14 +0200

    remove comments

--- ios/RNSslPinning/RNSslPinning.m
+++ ios/RNSslPinning/RNSslPinning.m
@@ -1,7 +1,4 @@
 //
-//  RNNativeFetch.m
-//  medipass
-//
 //  Created by Max Toyberman on 13/10/16.
 //  Copyright © 2016 Localz. All rights reserved.
 //
@@ -76,7 +73,7 @@ RCT_EXPORT_METHOD(removeCookieByName: (NSString *)cookieName
         NSHTTPURLResponse *httpResp = (NSHTTPURLResponse*) response;
         NSString *bodyString = [[NSString alloc] initWithData: responseObject encoding:NSUTF8StringEncoding];
         NSInteger statusCode = httpResp.statusCode;
-
+        
         if (!error) {
             // if(obj[@"responseType"]){
             NSString * responseType = obj[@"responseType"];
@@ -84,25 +81,25 @@ RCT_EXPORT_METHOD(removeCookieByName: (NSString *)cookieName
             if ([responseType isEqualToString:@"base64"]){
                 NSString* base64String = [responseObject base64EncodedStringWithOptions:0];
                 callback(@[[NSNull null], @{
-                                @"status": @(statusCode),
-                                @"headers": httpResp.allHeaderFields,
-                                @"data": base64String
-                                }]);
+                               @"status": @(statusCode),
+                               @"headers": httpResp.allHeaderFields,
+                               @"data": base64String
+                               }]);
             }
             else {
                 callback(@[[NSNull null], @{
-                                @"status": @(statusCode),
-                                @"headers": httpResp.allHeaderFields,
-                                @"bodyString": bodyString ? bodyString : @""           
-                                }]);
+                               @"status": @(statusCode),
+                               @"headers": httpResp.allHeaderFields,
+                               @"bodyString": bodyString ? bodyString : @""
+                               }]);
             }
         } else if (error && error.userInfo[AFNetworkingOperationFailingURLResponseDataErrorKey]) {
             dispatch_async(dispatch_get_main_queue(), ^{
                 callback(@[@{
-                    @"status": @(statusCode),
-                    @"headers": httpResp.allHeaderFields,
-                    @"bodyString": bodyString ? bodyString : @""
-                }, [NSNull null]]);
+                               @"status": @(statusCode),
+                               @"headers": httpResp.allHeaderFields,
+                               @"bodyString": bodyString ? bodyString : @""
+                               }, [NSNull null]]);
             });
         } else {
             dispatch_async(dispatch_get_main_queue(), ^{
@@ -132,38 +129,38 @@ RCT_EXPORT_METHOD(removeCookieByName: (NSString *)cookieName
     
     
     request = [[AFHTTPRequestSerializer serializer] multipartFormRequestWithMethod:@"POST" URLString:url parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData> _formData) {
-                        if([formData objectForKey:@"_parts"]){
-                            NSArray * parts = formData[@"_parts"];
-                            for (int i = 0; i < [parts count]; i++)
-                            {
-                                NSArray * part = parts[i];
-                                if([part[0] isKindOfClass:[NSString class]]) {
-                                    NSString * key = part[0];
-                                    if ([key isEqualToString:@"file"])
-                                    {
-                                        NSDictionary * value = part[1];
-                                        NSString * fileName = [value objectForKey:@"fileName"];
-                                        NSString * mimeType = [value objectForKey:@"type"];
-                                        NSString * data = [value objectForKey:@"data"];
-                                        NSData *nsdataFromBase64String = [[NSData alloc] initWithBase64EncodedString:data options:0];
-
-                                        [_formData appendPartWithFileData:nsdataFromBase64String name:@"file" fileName:fileName mimeType:mimeType];
-                                        //[_formData appendPartWithFileURL:[NSURL fileURLWithPath:path] name:name fileName:fileName mimeType:mimeType error:&error1];
-                                        //NSLog(@"%@",error1);
-
-                                    }
-                                    else  {
-                                        
-                                        NSString * value = part[1];
-                                        NSData *data = [value dataUsingEncoding:NSUTF8StringEncoding];
-                                        [_formData appendPartWithFormData:data name: key];
-                                    }
-                                }
-                            }
-                        }
+        if([formData objectForKey:@"_parts"]){
+            NSArray * parts = formData[@"_parts"];
+            for (int i = 0; i < [parts count]; i++)
+            {
+                NSArray * part = parts[i];
+                if([part[0] isKindOfClass:[NSString class]]) {
+                    NSString * key = part[0];
+                    if ([key isEqualToString:@"file"])
+                    {
+                        NSDictionary * value = part[1];
+                        NSString * fileName = [value objectForKey:@"fileName"];
+                        NSString * mimeType = [value objectForKey:@"type"];
+                        NSString * data = [value objectForKey:@"data"];
+                        NSData *nsdataFromBase64String = [[NSData alloc] initWithBase64EncodedString:data options:0];
+                        
+                        [_formData appendPartWithFileData:nsdataFromBase64String name:@"file" fileName:fileName mimeType:mimeType];
+                        //[_formData appendPartWithFileURL:[NSURL fileURLWithPath:path] name:name fileName:fileName mimeType:mimeType error:&error1];
+                        //NSLog(@"%@",error1);
+                        
+                    }
+                    else  {
+                        
+                        NSString * value = part[1];
+                        NSData *data = [value dataUsingEncoding:NSUTF8StringEncoding];
+                        [_formData appendPartWithFormData:data name: key];
+                    }
+                }
+            }
+        }
     } error:nil];
     
-
+    
     
     NSURLSessionUploadTask *uploadTask;
     uploadTask = [manager
@@ -178,7 +175,7 @@ RCT_EXPORT_METHOD(removeCookieByName: (NSString *)cookieName
                           NSLog(@"%@ %@", response, responseObject);
                           
                           NSHTTPURLResponse *httpResp = (NSHTTPURLResponse*) response;
-
+                          
                           NSString *bodyString = [[NSString alloc] initWithData: responseObject encoding:NSUTF8StringEncoding];
                           NSInteger statusCode = httpResp.statusCode;
                           
@@ -211,7 +208,7 @@ RCT_EXPORT_METHOD(fetch:(NSString *)url obj:(NSDictionary *)obj callback:(RCTRes
     
     AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
     manager.securityPolicy = policy;
-
+    
     manager.responseSerializer = [AFHTTPResponseSerializer serializer];
     
     
@@ -221,13 +218,13 @@ RCT_EXPORT_METHOD(fetch:(NSString *)url obj:(NSDictionary *)obj callback:(RCTRes
     if (obj[@"timeoutInterval"]) {
         [request setTimeoutInterval:[obj[@"timeoutInterval"] doubleValue] / 1000];
     }
-  
+    
     if(obj[@"headers"]) {
         [self setHeaders:obj request:request];
     }
-
+    
     if (obj) {
-
+        
         if ([obj objectForKey:@"body"]) {
             NSDictionary * body = obj[@"body"];
             
@@ -238,7 +235,7 @@ RCT_EXPORT_METHOD(fetch:(NSString *)url obj:(NSDictionary *)obj callback:(RCTRes
                 [self performMultipartRequest:manager obj:obj url:url request:request callback:callback formData:formData];
             }
             else {
-
+                
                 // post a string
                 NSData *data = [obj[@"body"] dataUsingEncoding:NSUTF8StringEncoding];
                 [request setHTTPBody:data];
